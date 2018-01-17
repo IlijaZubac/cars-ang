c2886e658930cca43c3e384409823db407182c09
IlijaZubac <ilija.zubac@gmail.com>
2018-01-17 22:14:32 +0100

    6 commit

--- src/app/car-form-component/car-form-component.component.ts
+++ src/app/car-form-component/car-form-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../modules/car';
+
 
 @Component({
   selector: 'app-car-form-component',
@@ -7,13 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarFormComponentComponent implements OnInit {
   year;
+  private cars = [];
+  public newCar;
   constructor() { 
     this.year=[1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,
       2000,2001,200,22003,2004,2005,2006,2007,2008,2009,2010,2011,
       2012,2013,2014,2015,2016,2017,2018];
   }
   submitCar(){
-    
+
   }
   ngOnInit() {
   }
