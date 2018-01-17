798b44d6bcd1e8c9308376eee884909c784adf44
IlijaZubac <ilija.zubac@gmail.com>
2018-01-17 22:06:02 +0100

    5 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
  
 
 import { AppComponent } from './app.component';
@@ -22,8 +23,7 @@ import { CarFormComponentComponent } from './car-form-component/car-form-compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-   
- 
+    FormsModule
      
   ],
   providers: [CarServiceService],
